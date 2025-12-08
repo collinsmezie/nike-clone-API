841f6ef6c348831ded1135ebed1209794a966cdd
collinsmezie <collinschimezieobinwa@gmail.com>
2025-12-08 16:47:06 +0200

    refactor: Remove PrismaLibSql adapter initialization from PrismaService.

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,18 +1,37 @@
+// import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+// import { PrismaClient } from '@prisma/client';
+// import { PrismaLibSql } from '@prisma/adapter-libsql';
+
+// @Injectable()
+// export class PrismaService
+//   extends PrismaClient
+//   implements OnModuleInit, OnModuleDestroy
+// {
+//   constructor() {
+//     const adapter = new PrismaLibSql({
+//       url: process.env.DATABASE_URL || 'file:./dev.db',
+//     });
+//     super({ adapter });
+//   }
+//   async onModuleInit() {
+//     await this.$connect();
+//   }
+
+//   async onModuleDestroy() {
+//     await this.$disconnect();
+//   }
+// }
+
+
+
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { PrismaLibSql } from '@prisma/adapter-libsql';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
-  constructor() {
-    const adapter = new PrismaLibSql({
-      url: process.env.DATABASE_URL || 'file:./dev.db',
-    });
-    super({ adapter });
-  }
   async onModuleInit() {
     await this.$connect();
   }
